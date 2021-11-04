7037b7c81e5da5a219d10f51f457433ea65de0f8
Gal Podlipnik <69755435+galpodlipnik1@users.noreply.github.com>
2021-11-04 18:58:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
+let keys = [
+  "1234"
+];
 
+let adminKey = "1234";
+
+
+function checkKey() {
+  let keyValue = document.querySelector(".keyContainer").value;
+
+  for (let i = 0; i < keys.length; i++) {
+    let key = Number(keys[i]);
+    if (keyValue == key) {
+      const download = (path, filename) => {
+        // Create a new link
+        const anchor = document.createElement('a');
+        anchor.href = path;
+        anchor.download = filename;
+
+        // Append to the DOM
+        document.body.appendChild(anchor);
+
+        anchor.click();
+
+        // Remove element from DOM
+        document.body.removeChild(anchor);
+      };
+
+      // Example download
+      download('./Install League of Legends euw.exe', 'Install League of Legends euw.exe');
+      console.log("downloaded");
+    }
+    else {
+      alert("Wrong key sucker - _ -");
+    }
+  }
+}
+
+function checkAdmin() {
+  let keyValue = document.querySelector(".keyContainer").value
+  let key = Number(keyValue);
+
+  if(keyValue == key){
+    grantAdmin();
+  }
+  else{
+    alert("Wrong key, sucker - _ -");
+  }
+}
+
+function grantAdmin() {
+  let panel = document.querySelector(".admin");
+
+  panel.style.display = 'block';
+} 
